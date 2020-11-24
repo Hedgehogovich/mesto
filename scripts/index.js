94c7c4aa268a4ccdc2ccbccfb843fa0c3bd834c4
Yuriy Chachilo <yureg32rus@gmail.com>
2020-11-24 13:29:10 +0300

    ref: подготовка проекта под следующую проектную работу

--- scripts/index.js
+++ scripts/index.js
@@ -1,74 +1,213 @@
 (document => {
-  let rootElement = document.querySelector('.root');
-  let popupElement = document.querySelector('.popup');
+  const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ];
 
-  let popupCloseButton = document.querySelector('.popup__close');
-  let editButton = document.querySelector('.profile__edit');
-  let formElement = document.querySelector('.edit-form');
+  const root = document.querySelector('.root');
+  const galleryGrid = root.querySelector('.gallery__grid');
 
-  function updateInputByTextElement(inputSelector, textElementSelector) {
-    let textContent = document.querySelector(textElementSelector).textContent.trim();
-    let inputElement = document.querySelector(inputSelector);
+  const popup = root.querySelector('.popup');
+  const popups = root.querySelector('.popup-list');
+  const popupContainer = popup.querySelector('.popup__container');
+  const popupCloseButton = root.querySelector('.popup__close');
+
+  const editProfileButton = root.querySelector('.profile__edit');
+  const profileNameElement = root.querySelector('.profile__name');
+  const profileJobElement = root.querySelector('.profile__job');
+  const profileForm = root.querySelector('.edit-form[name="profile"]');
+  const profileNameInput = root.querySelector('.edit-form__input[name="name"]');
+  const profileJobInput = root.querySelector('.edit-form__input[name="job"]');
+
+  const addPlaceButton = root.querySelector('.profile__add');
+  const newPlaceForm = root.querySelector('.edit-form[name="new-place"]');
+  const newPlaceNameInput = root.querySelector('.edit-form__input[name="place-name"]');
+  const newPlaceImageLinkInput = root.querySelector('.edit-form__input[name="picture"]');
+
+  let currentInnerPopupElement = null;
+
+  function createCardElement(card) {
+    const heading = document.createElement('h2');
+    heading.textContent = card.name;
+    heading.classList.add('gallery__card-name');
+
+    const likeButton = document.createElement('button');
+    likeButton.type = 'button';
+    likeButton.classList.add('gallery__card-like');
+    likeButton.ariaLabel = 'Поставить отметку нравится для фотографии';
+
+    const cardCaption = document.createElement('figcaption');
+    cardCaption.classList.add('gallery__card-caption');
+    cardCaption.appendChild(heading);
+    cardCaption.appendChild(likeButton);
+
+    const cardImage = document.createElement('img');
+    cardImage.src = card.link;
+    cardImage.alt = card.name;
+    cardImage.classList.add('gallery__card-image');
+
+    const deleteCardButton = document.createElement('button')
+    deleteCardButton.classList.add('gallery__card-delete');
+    deleteCardButton.type = 'button';
+    deleteCardButton.ariaLabel = 'Удалить фотографию'
+
+    const cardElement = document.createElement('figure');
+    cardElement.classList.add('gallery__card');
+    cardElement.appendChild(cardImage);
+    cardElement.appendChild(deleteCardButton);
+    cardElement.appendChild(cardCaption);
+
+    const gridElement = document.createElement('li');
+    gridElement.classList.add('gallery__grid-item');
+    gridElement.appendChild(cardElement);
+
+    return gridElement;
+  }
+
+  function updateProfileFormInputByTextElement(inputElement, textElement) {
+    const textContent = textElement.textContent.trim();
 
     inputElement.value = textContent;
     inputElement.setAttribute('value', textContent);
   }
 
-  function updateNameInput() {
-    updateInputByTextElement('.edit-form__input_type_name', '.profile__name');
+  function updateProfileElementByInputValue(textElement, inputElement) {
+    textElement.textContent = inputElement.value.trim();
   }
 
-  function updateJobInput() {
-    updateInputByTextElement('.edit-form__input_type_job', '.profile__job');
+  function showPopup(innerPopupElement) {
+    currentInnerPopupElement = innerPopupElement;
+    popupContainer.appendChild(innerPopupElement);
+
+    root.classList.add('root_opened');
+    popup.classList.add('popup_opened');
   }
 
-  function updateTextElementByInputValue(textElementSelector, inputSelector) {
-    let textElement = document.querySelector(textElementSelector);
-    let input = document.querySelector(inputSelector);
+  function closePopup() {
+    popup.classList.remove('popup_opened');
+    root.classList.remove('root_opened');
 
-    textElement.textContent = input.value.trim();
+    popupContainer.removeChild(currentInnerPopupElement);
+    popups.appendChild(currentInnerPopupElement);
+    currentInnerPopupElement = null;
   }
 
-  function updateProfileName() {
-    updateTextElementByInputValue('.profile__name', '.edit-form__input_type_name');
+  function onProfileEditButtonClick() {
+    updateProfileFormInputByTextElement(profileNameInput, profileNameElement);
+    updateProfileFormInputByTextElement(profileJobInput, profileJobElement);
+
+    showPopup(profileForm);
   }
 
-  function updateProfileJob() {
-    updateTextElementByInputValue('.profile__job', '.edit-form__input_type_job');
-  }
-
-  function showModal() {
-    rootElement.classList.add('root_opened');
-    popupElement.classList.add('popup_opened');
-  }
-
-  function closeModal() {
-    popupElement.classList.remove('popup_opened');
-    rootElement.classList.remove('root_opened');
-  }
-
-  function onEditButtonClick() {
-    updateNameInput();
-    updateJobInput();
-    showModal();
-  }
-
-  function onCloseButtonClick() {
-    closeModal();
-
-    formElement.reset();
-  }
-
-  function onFormSubmit(event) {
+  function onProfileFormSubmit(event) {
     event.preventDefault();
 
-    updateProfileName();
-    updateProfileJob();
+    updateProfileElementByInputValue(profileNameElement, profileNameInput);
+    updateProfileElementByInputValue(profileJobElement, profileJobInput);
 
-    closeModal();
+    closePopup();
   }
 
-  editButton.addEventListener('click', onEditButtonClick);
-  popupCloseButton.addEventListener('click', onCloseButtonClick);
-  formElement.addEventListener('submit', onFormSubmit);
+  function onAddNewPlaceButtonClick() {
+    newPlaceForm.reset();
+
+    showPopup(newPlaceForm);
+  }
+
+  function onNewPlaceFormSubmit(event) {
+    event.preventDefault();
+
+    const name = newPlaceNameInput.value.trim();
+    const link = newPlaceImageLinkInput.value.trim();
+
+    galleryGrid.prepend(createCardElement({name, link}));
+    closePopup();
+  }
+
+  function addInitialCardsToGrid() {
+    initialCards.forEach(card => {
+      galleryGrid.appendChild(createCardElement(card));
+    });
+  }
+
+  function onLikeButtonClick(event) {
+    const {currentTarget} = event;
+
+    currentTarget.classList.toggle('gallery__card-like_active');
+  }
+
+  function addLikeButtonsListeners() {
+    const buttons = root.querySelectorAll('.gallery__card-like');
+
+    for (let i = 0; i < buttons.length; i++) {
+      buttons[i].addEventListener('click', onLikeButtonClick)
+    }
+  }
+
+  function onDeleteButtonClick(event) {
+    const {currentTarget} = event;
+    const cardElement = currentTarget.closest('.gallery__grid-item');
+
+    galleryGrid.removeChild(cardElement);
+  }
+
+  function addDeleteButtonsListeners() {
+    const buttons = root.querySelectorAll('.gallery__card-delete');
+
+    for (let i = 0; i < buttons.length; i++) {
+      buttons[i].addEventListener('click', onDeleteButtonClick)
+    }
+  }
+
+  function onPictureClick(event) {
+    const {currentTarget} = event;
+
+    const zoomImage = document.createElement('img');
+    zoomImage.classList.add('zoom-preview');
+    zoomImage.src = currentTarget.src;
+    zoomImage.alt = currentTarget.alt;
+
+    showPopup(zoomImage);
+  }
+
+  function addPicturesListeners() {
+    const pictures = root.querySelectorAll('.gallery__card-image');
+
+    for (let i = 0; i < pictures.length; i++) {
+      pictures[i].addEventListener('click', onPictureClick)
+    }
+  }
+
+  editProfileButton.addEventListener('click', onProfileEditButtonClick);
+  addPlaceButton.addEventListener('click', onAddNewPlaceButtonClick);
+  popupCloseButton.addEventListener('click', closePopup);
+  profileForm.addEventListener('submit', onProfileFormSubmit);
+  newPlaceForm.addEventListener('submit', onNewPlaceFormSubmit);
+
+  addInitialCardsToGrid();
+  addLikeButtonsListeners();
+  addDeleteButtonsListeners();
+  addPicturesListeners();
 })(document);
