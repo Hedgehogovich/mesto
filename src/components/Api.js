d04baa7d346ba9daaa5345741d828d29afc62a85
Yuriy Chachilo <yureg32rus@gmail.com>
2021-03-01 15:05:12 +0300

    fix: фикс выбрасывания исключения при ошибочном ответе для метода _getOkStatusFromResponse

--- src/components/Api.js
+++ src/components/Api.js
@@ -4,45 +4,6 @@ export default class Api {
     this._headers = headers;
   }
 
-  _makeRequest({
-    action,
-    method = 'GET',
-    data = null,
-  }) {
-    if (!action) {
-      return Promise.reject('Не передано адреса запроса');
-    }
-
-    const requestParams = {
-      method,
-      headers: this._headers,
-    };
-
-    let url = this._baseUrl + action;
-
-    if (data) {
-      if (method === 'GET') {
-        url += new URLSearchParams(data).toString();
-      } else {
-        requestParams.body = JSON.stringify(data);
-      }
-    }
-
-    return fetch(url, requestParams);
-  }
-
-  _getJsonFromResponse(response) {
-    if (!response.ok) {
-      return Promise.reject(`Ошибка: ${response.status}`)
-    }
-
-    return response.json();
-  }
-
-  _getOkStatusFromResponse(response) {
-    return response.ok;
-  }
-
   getAuthorizedUserInfo() {
     return this._makeRequest({
       action: '/users/me'
@@ -98,5 +59,45 @@ export default class Api {
       method: 'PATCH',
       data: {avatar}
     }).then(this._getJsonFromResponse);
+  }
+
+
+  _makeRequest({
+                 action,
+                 method = 'GET',
+                 data = null,
+               }) {
+    if (!action) {
+      return Promise.reject('Не передано адреса запроса');
+    }
+
+    const requestParams = {
+      method,
+      headers: this._headers,
+    };
+
+    let url = this._baseUrl + action;
+
+    if (data) {
+      if (method === 'GET') {
+        url += new URLSearchParams(data).toString();
+      } else {
+        requestParams.body = JSON.stringify(data);
+      }
+    }
+
+    return fetch(url, requestParams);
+  }
+
+  _getJsonFromResponse(response) {
+    if (!response.ok) {
+      return Promise.reject(`Ошибка: ${response.status}`);
+    }
+
+    return response.json();
+  }
+
+  _getOkStatusFromResponse(response) {
+    return response.ok || Promise.reject(`Ошибка: ${response.status}`);
   }
 }
